ba12d0053720cfd5f96d2d8cf0b9e354780154ad
Lucas Franca <lucascfranca11@gmail.com>
2023-08-31 23:56:01 -0300

    Limpeza de imports

--- src/main/java/com/products/EcommerceApplication.java
+++ src/main/java/com/products/EcommerceApplication.java
@@ -2,12 +2,6 @@ package com.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-/**
- * Hello world!
- *
- */
 
 @SpringBootApplication
 public class EcommerceApplication {
--- src/main/java/com/products/model/Manga.java
+++ src/main/java/com/products/model/Manga.java
@@ -1,6 +1,5 @@
 package com.products.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Data;
